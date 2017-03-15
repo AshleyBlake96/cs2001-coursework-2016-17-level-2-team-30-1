9f623a7b6adc0bf07b4fd7b3c07f9a85468a81c6
1507461 <1507461@my.brunel.ac.uk>
2017-03-15 16:12:22 +0000

    I initialised the first EditText which would be used as  part of the same of the image when stored in the database and when the browser button is pressed it runs the open gallery method if the first edit text is not empty and it opens the gallery of the image

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/CreateEventActivity.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/CreateEventActivity.java
@@ -1,10 +1,14 @@
 package com.app.groupproject.officialunisocial.ActivityPages;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.app.groupproject.officialunisocial.R;
 
@@ -29,7 +33,28 @@ public class CreateEventActivity extends AppCompatActivity {
 
         eventImage = (ImageView) findViewById(R.id.imageview_id);
         Button browserImageBtn = (Button) findViewById(R.id.BrowseImage_button_id);
+        eventDetails[0] = (EditText) findViewById(R.id.Title_field_id);
+
+        browserImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(textToString(eventDetails[0]).equals("")) {
+                    Toast.makeText(CreateEventActivity.this, "Fill in Title Field", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    openGallery();
+                }
+            }
+        });
 
 
     }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+
 }
