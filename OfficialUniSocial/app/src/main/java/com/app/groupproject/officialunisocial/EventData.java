5de3e9c8d7ddda5d41ee9fbd63cbb1ff8ea7361f
1507461 <1507461@my.brunel.ac.uk>
2017-03-15 15:13:27 +0000

    Added an empty constructor as it is required for the database when info is collected and I also added the getters and setters

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/EventData.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/EventData.java
@@ -7,6 +7,9 @@ public class EventData {
     private String image, title, description, host, date, time, location, noStudents;
 
 
+    public EventData(){
+
+    }
 
     public EventData(String image, String title, String description, String host, String location, String noStudents, String date, String time) {
         this.image = image;
@@ -18,6 +21,71 @@ public class EventData {
         this.noStudents = noStudents;
         this.date = date;
         this.time = time;
+    }
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String desc) {
+        this.description = desc;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNoStudents() {
+        return noStudents;
+    }
+
+    public void setNoStudents(String noStudents) {
+        this.noStudents = noStudents;
     }
 
 
