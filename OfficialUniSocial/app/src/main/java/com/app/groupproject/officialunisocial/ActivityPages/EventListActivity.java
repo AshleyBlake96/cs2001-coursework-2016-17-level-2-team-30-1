4564dcd8c94f05aa1d4a48df610885b7b8a96e46
1507461 <1507461@my.brunel.ac.uk>
2017-03-15 16:51:52 +0000

    Initialised the Toolbar and the DrawerLayout

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/EventListActivity.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/EventListActivity.java
@@ -1,7 +1,9 @@
 package com.app.groupproject.officialunisocial.ActivityPages;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.app.groupproject.officialunisocial.EventData;
 import com.app.groupproject.officialunisocial.R;
@@ -21,6 +23,11 @@ public class EventListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 
     }
