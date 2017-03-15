694bbe710f560263c6bbeaf10ec5e8e1fb3aeab3
1507461 <1507461@my.brunel.ac.uk>
2017-03-15 15:44:13 +0000

    got the resources from the xml file and stored it in the correct variables eg ImageView and TextView

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/ProfileActivity.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/ProfileActivity.java
@@ -2,6 +2,8 @@ package com.app.groupproject.officialunisocial.ActivityPages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.app.groupproject.officialunisocial.R;
 
@@ -12,7 +14,14 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-
+        ImageView profileImage = (ImageView) findViewById(R.id.profilepage_image_id);
+        TextView username = (TextView) findViewById(R.id.profilepage_username_id);
+        TextView name = (TextView) findViewById(R.id.profilepage_users_name_id);
+        TextView email = (TextView) findViewById(R.id.profilepage_emailInfo_id);
+        TextView gender = (TextView) findViewById(R.id.profilepage_genderInfo_id);
+        TextView age = (TextView) findViewById(R.id.profilepage_ageInfo_id);
+        TextView uni = (TextView) findViewById(R.id.profilepage_universityInfo_id);
+        TextView number = (TextView) findViewById(R.id.profilepage_numPhoneInfo_id);
 
 
     }
