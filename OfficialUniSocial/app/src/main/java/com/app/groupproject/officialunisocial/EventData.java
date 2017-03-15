942fba45b3153fb4c580677ac0b1e8b541a02d5b
1507461 <1507461@my.brunel.ac.uk>
2017-03-15 15:12:32 +0000

    Added the EventData variables and constructor that stores the event information when collected

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/EventData.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/EventData.java
@@ -4,10 +4,20 @@ package com.app.groupproject.officialunisocial;
 
 public class EventData {
 
-    public EventData(){
+    private String image, title, description, host, date, time, location, noStudents;
 
-        int test = 1;
 
+
+    public EventData(String image, String title, String description, String host, String location, String noStudents, String date, String time) {
+        this.image = image;
+        this.description = description;
+        this.title = title;
+        this.description = description;
+        this.host = host;
+        this.location = location;
+        this.noStudents = noStudents;
+        this.date = date;
+        this.time = time;
     }
 
 
