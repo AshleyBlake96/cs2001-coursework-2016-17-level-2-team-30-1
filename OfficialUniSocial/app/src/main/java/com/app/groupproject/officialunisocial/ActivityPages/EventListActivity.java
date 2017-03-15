72ec45795edc09a3b2f7d6366b10645073839a61
1507461 <1507461@my.brunel.ac.uk>
2017-03-15 16:50:37 +0000

    added the variables pressedEvent for the pressed event. eventList for the complete list of events and userInfo for the users info

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/EventListActivity.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/EventListActivity.java
@@ -3,13 +3,25 @@ package com.app.groupproject.officialunisocial.ActivityPages;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.app.groupproject.officialunisocial.EventData;
 import com.app.groupproject.officialunisocial.R;
 
+import java.util.ArrayList;
+
 public class EventListActivity extends AppCompatActivity {
+
+    private ArrayList<EventData> eventList = new ArrayList<>();
+    private EventData pressedEvent;
+    private UserData userInfo;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+
     }
 }
