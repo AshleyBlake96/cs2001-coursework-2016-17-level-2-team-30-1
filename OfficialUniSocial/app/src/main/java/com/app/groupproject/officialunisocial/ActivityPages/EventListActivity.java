82e279b84906a9c3a524b78d59ff479f390a4715
1507461 <1507461@my.brunel.ac.uk>
2017-03-15 16:53:25 +0000

    added and initialised the ActionBarDrawerToggle and added a drawer listener to check if the drawer was pressed and I synced the state for the ActionBarDrawerToggle

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/EventListActivity.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/EventListActivity.java
@@ -1,6 +1,7 @@
 package com.app.groupproject.officialunisocial.ActivityPages;
 
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -28,6 +29,10 @@ public class EventListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
 
     }
