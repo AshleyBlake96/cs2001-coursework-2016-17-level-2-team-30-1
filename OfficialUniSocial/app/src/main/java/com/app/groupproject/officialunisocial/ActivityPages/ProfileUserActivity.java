24c65c09956ffdcfdb3c2a1b84de5967c8d1265a
mehdij123 <1502426@my.brunel.ac.uk>
2017-03-22 18:41:12 +0000

    Assigned Variables to XML id's

--- OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/ProfileUserActivity.java
+++ OfficialUniSocial/app/src/main/java/com/app/groupproject/officialunisocial/ActivityPages/ProfileUserActivity.java
@@ -11,5 +11,18 @@ import com.app.groupproject.officialunisocial.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileUserActivity extends AppCompatActivity {
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ImageView profileImage = (ImageView) findViewById(R.id.profilepage_image_id);
+        TextView username = (TextView) findViewById(R.id.profilepage_username_id);
+        TextView name = (TextView) findViewById(R.id.profilepage_users_name_id);
+        TextView email = (TextView) findViewById(R.id.profilepage_emailInfo_id);
+        TextView gender = (TextView) findViewById(R.id.profilepage_genderInfo_id);
+        TextView age = (TextView) findViewById(R.id.profilepage_ageInfo_id);
+        TextView uni = (TextView) findViewById(R.id.profilepage_universityInfo_id);
+        TextView number = (TextView) findViewById(R.id.profilepage_numPhoneInfo_id);
+
 }
